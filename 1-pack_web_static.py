fd6e38a965dd70ef51399a81b9023f04e3ac2a2c
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-17 22:45:22 +0000

    Add task 1: Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" 1. Compress before sending
+script that generates a .tgz archive from thecontents of the web_static
+"""
+from fabric.api import *
+import datetime
+
+
+def do_pack():
+    """ """
+    local("mkdir -p ./versions")
+    date = datetime.datetime.now()
+    date_format = date.strftime("%Y%m%d%H%M%S")
+    new_archive = local("tar -cvzf versions/web_static_{}.tgz web_static".format(date_format))
+    if new_archive.succeeded:
+        return "versions/web_static_{}.tgz web_static".format(date_format)
+    else:
+        return None
