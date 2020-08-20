58ad4384ac7d9b5e03e256d524b1c3be397cd08e
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-20 21:55:07 +0000

    task 2 wrong

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,64 +3,49 @@
 Fabric script that distributes an archive to web servers,
 using the function do_deploy
 """
-from fabric.api import *
+from fabric.operations import local, run, put, env
 from os import path
+
+
+env.hosts = ['35.237.202.116', '34.73.195.113']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
     """ Deploy archive """
 
-    env.user = 'ubuntu'
-    env.hosts = ['35.237.202.116', '34.73.195.113']
-
     if not path.exists(archive_path):
         return False
 
     # send archive
-    result = put(archive_path, "/tmp/")
-    if result.failed:
-        return False
+    put(archive_path, "/tmp/")
 
     # save names
     arch = archive_path.split('/')[1].split('.')[0]
-    archDir = "/data/web_static/releases/".apend(arch)
+    archDir = "/data/web_static/releases/" + arch
     remote_path = "/temp/{}".format(archive_path.split('/')[1])
 
     # make dir
-    run("mkdir -p /data/web_static/releases/{}".format(arch))
+    run("mkdir -p {}".format(archDir))
 
     # uncompress the archive
     # -x: Extract files from an archive
     # -z: Uncompress whit gzip command
     # -f: use archive file or device ARCHIVE
     # -C: Change to directory.
-    result = run(
-        "tar -xzf {} -C {}".format(remote_path, archDir))
-    if result.failed:
-        return False
+    run("tar -xzf {} -C {}/".format(remote_path, archDir))
 
     # delete the archive
-    result = run("rm {}".format(remote_path))
-    if result.failed:
-        return False
+    run("rm {}".format(remote_path))
 
     # move files
-    result = run("mv {}/web_static/* {}".format(archDir, archDir))
-    if result.failed:
-        return False
+    run("mv {}/web_static/* {}".format(archDir, archDir))
 
-    result = run("rm -rf {}/web_static".format(archDir))
-    if result.failed:
-        return False
+    run("rm -rf {}/web_static".format(archDir))
 
     # softlink to new deploy
-    result = run("rm - rf / data/web_static/current")
-    if result.failed:
-        return False
+    run("rm - rf /data/web_static/current")
 
-    result = run(
-        "ln -sf {} /data/web_static/current".format(archDir))
-    if result.failed:
-        return False
+    run("ln -s {}/ /data/web_static/current".format(archDir))
 
     return True
