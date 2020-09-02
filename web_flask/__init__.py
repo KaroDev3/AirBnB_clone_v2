354fb0fb7aa35a5ed10ebe1b41579ad716853ae1
Diana Carolina Quintero <karodev3@gmail.com>
2020-09-02 02:15:04 +0000

    Add task 0: Hello Flask! (0-hello_route.py, __init__.py)

--- web_flask/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Initialize the flask class"""
-from flask import Flask
-import sys
-
-app = Flask(__name__)
-
-cli = sys.modules['flask.cli']
-cli.show_server_banner = lambda *x: None
