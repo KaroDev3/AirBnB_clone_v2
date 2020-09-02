4e6af727d46f05de61a50a5466f459777e7ed772
Diana Carolina Quintero <karodev3@gmail.com>
2020-09-02 01:27:00 +0000

    Add task 0: Hello Flask! (0-hello_route.py, __init__.py)

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+0. Hello Flask!
+Script that starts a Flask web application.
+$ export FLASK_APP=0-hello_route.py
+python3 -m web_flask.0-hello_route
+"""
+from web_flask import app
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+	"""response text"""
+	return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ web_flask/__init__.py
@@ -0,0 +1,4 @@
+"""Initialize the flask class"""
+from flask import Flask
+app = Flask(__name__)
+
